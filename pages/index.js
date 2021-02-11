51156e616b8fa341e0e48804da7b88beb1b5819c
frasnym <frastyawan.nym@gmail.com>
2021-02-11 14:03:04 +0800

    feat(index): Add input element

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import Layout from '../components/layout/Layout';
 import { getEmojiList } from '../libs/sheets';
 
+// width: 100%;
+// line-height: 1.8rem;
+// font-size: 1.2rem;
+// color: var(--color-black);
+// border-radius: var(--border-radius-md);
+// border: 2px solid var(--color-gray-3);
+// background: var(--color-gray-2);
+// outline: none;
+
+// border-color: rgba(var(--color-brand-rgb),.5);
+// background: var(--color-gray-0);
+
 export default function IndexPage({ emojis }) {
 	return (
 		<Layout>
+			<input
+				className="w-full text-xl rounded-md border-2 border-solid outline-none bg-gray-200 border-gray-300 text-black focus:bg-gray-50 focus:border-gray-200 p-3"
+				placeholder="🔎 Search for an Emoji"
+			/>
 			{emojis[0].title}
-			input
 		</Layout>
 	);
 }
